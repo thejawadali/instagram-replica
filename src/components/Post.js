9074d43d1692367095bf3657d2c710c2ebf3e87f
Jawad Ali <alijamil734@gmail.com>
2021-11-15 23:44:25 +0500

    share post

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,16 +3,50 @@ import { FiHeart, FiSend } from "react-icons/fi"
 import { MdBookmarkBorder } from "react-icons/md"
 import { getFirstLetterOfUserName } from "../utils"
 import Avatar from "./Avatar"
+import { addDoc, collection, serverTimestamp } from "firebase/firestore"
+import db from "../firebase"
+import { useState } from "react"
 
 
-function Post ({post}) {
-  
+function Post ( { post } ) {
+
+  const [isActive, setIsActive] = useState( false )
+  const [caption, setCaption] = useState( "" )
+
+  function closeModal () {
+    setIsActive( false )
+    setCaption("")
+  }
+  function openModal () {
+    if ( post.userName === localStorage.getItem( "userName" ) ) {
+      alert( "Can not share" )
+      return
+    }
+    setIsActive( true )
+  }
+
+  async function share (e) {
+    e.preventDefault()
+    try {
+      await addDoc( collection( db, "insta-posts" ), {
+        createdAt: serverTimestamp(),
+        caption,
+        image: post.image,
+        userName: localStorage.getItem( "userName" ),
+        profilePic: localStorage.getItem( "profilePic" )
+      } )
+      closeModal()
+    } catch ( e ) {
+      console.error( e.message )
+    }
+  }
+
   return (
     <div className="border bg-white my-10">
       {/* Post Header */}
       <div className="flex items-center px-4 py-3">
         {/* Avatar */}
-        <Avatar letter={getFirstLetterOfUserName(post.userName)} image={post.profilePic} />
+        <Avatar letter={getFirstLetterOfUserName( post.userName )} image={post.profilePic} />
         <div className="flex flex-col mx-3 ">
           <h2 className="cursor-pointer text-lg text-bold">{post.userName}</h2>
           <p className="text-xs cursor-pointer">{post.city}</p>
@@ -25,9 +59,9 @@ function Post ({post}) {
         <div className="flex ">
           <FiHeart className="cursor-pointer w-6 h-6 font-thin mx-2" />
           <FaRegComment className="cursor-pointer w-6 h-6 font-thin mx-2" />
-          <FiSend className="cursor-pointer w-6 h-6 font-thin mx-2" />
+          <FiSend onClick={openModal} className="cursor-pointer w-6 h-6 font-thin mx-2" />
         </div>
-          <MdBookmarkBorder className="cursor-pointer w-7 h-7 font-thin mx-2" />
+        <MdBookmarkBorder className="cursor-pointer w-7 h-7 font-thin mx-2" />
       </div>
       {/* likes */}
       <p className="text-sm font-bold mx-4 mb-2">{post.likes || "0"} likes</p>
@@ -39,11 +73,65 @@ function Post ({post}) {
       <p className="text-sm ml-4"><strong className="mr-1">Hammad Asif</strong>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias quae ullam magni saepe dolorem, esse hic, illum ipsam, dolore temporibus aspernatur cumque quas ducimus. Voluptas deserunt asperiores praesentium eos laudantium.</p>
 
       {/* Add Comment */}
-      <form onSubmit={(e) => {e.preventDefault();}} className="w-full flex px-4 border-t mt-4">
+      <form onSubmit={( e ) => { e.preventDefault() }} className="w-full flex px-4 border-t mt-4">
         <input className="flex-1 outline-none py-3 bg-transparent" type="text" placeholder="Add a comment..." />
         <button type="submit" className="text-blue-600 font-semibold">Post</button>
       </form>
+
+      {/* Modal to share post */}
+
+      {isActive ? (
+        <>
+          <div
+            id="backdrop"
+            onClick={( e ) => {
+              if ( e.target.id === "backdrop" ) {
+                closeModal()
+              }
+            }}
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative my-6 mx-auto w-11/12 md:w-4/5 lg:w-1/2">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="text-center py-4 border-b border-solid border-gray-200 rounded-t">
+                  <h3 className="text-xl  font-semibold">
+                    Share Post
+                  </h3>
+                </div>
+
+                {/*body*/}
+                <div className="relative p-6 flex justify-center">
+                  <form onSubmit={share} className="mt-8 text-right">
+                    <textarea value={caption} onChange={( e ) => { setCaption( e.target.value ) }} name="" id="" placeholder="Change caption" cols="30" rows="3" className="resize-none my-1 border w-full px-2 py-1 outline-none focus:ring-1 rounded"></textarea>
+                    <button type="submit" className="bg-blue-500 px-3 py-1 rounded text-white hover:bg-blue-600">Share</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-80 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
+
+
   )
 }
 
